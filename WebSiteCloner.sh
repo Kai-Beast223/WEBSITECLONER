169e55f0c0d365a3168452fe77c768055ab5e6cc
H4MST3R <53465855+Kai-Beast223@users.noreply.github.com>
2019-10-25 09:17:55 -0700

    Update WebSiteCloner.sh

--- WebSiteCloner.sh
+++ WebSiteCloner.sh
@@ -8,17 +8,24 @@ echo -e "${RED}-=-=-=FUCK NU----LL BYTE=-=-=-"
 echo -e "${RED}-=-=-=WEBSITE-----CLONER=-=-=-"
 echo -e "${RED}-=-=-=-=-=By H4MST3R=-=-=-=-=-"
 echo -e "${RED}-=-=-=-=-=-=-=++=-=-=-=-=-=-=-"
+
 echo -e "${RED}Engading Clean-Up Scripts"
 cd $1
 rm -rf $2
 mkdir $2
+
 echo -e "${RED}Clean-Up Done"
 cd $2
+
 echo -e "${RED}Starting up Download"
 echo -e "${RED}-=-=-=-=-=-=-=++=-=-=-=-=-=-=-"
+
 sleep 3s
 wget -r --no-parent $3
-echo -e "${RED}-=-=-=-=-=-=-=++=-=-=-=-=-=-=-"
+
+sleep 3s
+echo -e "${RED}=-=-=-=-=-=-=-=-=-=++=-=-=-=-=-=-=-=-=-="
 echo -e "${RED}DONE LOCAL BACKUP SAVED ENJOY YOUR FILES"
-echo -e "${RED}WEBSITE PWNED AND FUCKED"
-echo -e "${RED}-=-=-=-=-=-=-=++=-=-=-=-=-=-=-"
+echo -e "${RED}~~~~~~~~WEBSITE PWNED AND FUCKED~~~~~~~~"
+echo -e "${RED}~~~~~~~~~~~~~~~BY H4MST3R~~~~~~~~~~~~~~~"
+echo -e "${RED}=-=-=-=-=-=-=-=-=-=++=-=-=-=-=-=-=-=-=-="
